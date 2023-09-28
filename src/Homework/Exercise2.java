0c5a47a5f680ab67d9f625c5293575db4324f75f
sers1 <sers156@mail.ru>
2023-09-28 12:42:26 +1000

    hw done

--- src/Homework/Exercise2.java
+++ src/Homework/Exercise2.java
@@ -2,6 +2,17 @@ package Homework;
 
 public class Exercise2 {
     public static void main(String[] args) {
-
+        float f1=27.24F;
+        System.out.println(f1);
+        float f2=20;
+        System.out.println(f2);
+        double d1=23D;
+        System.out.println(d1);
+        double d2=25;
+        System.out.println(d2);
+        boolean bool1=true;
+        System.out.println(bool1);
+        boolean bool2=false;
+        System.out.println(bool2);
     }
 }
--- src/Homework/Exercise3.java
+++ src/Homework/Exercise3.java
@@ -2,6 +2,12 @@ package Homework;
 
 public class Exercise3 {
     public static void main(String[] args) {
+        char c1='b';
+        System.out.println(c1);
+        char c2= 1000;
+        System.out.println(c2);
+        char c3='\u01AB';
+        System.out.println(c3);
 
     }
 }
